e7d792fbfb52b10bb22cd23c47bddfb7d8128ffb
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2025-08-11 16:02:35 +0530

    manju bro got added

---
+++ MockTask/src/test/java/ManjuBro.java
@@ -0,0 +1,12 @@
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.testng.annotations.Test;
+
+public class ManjuBro {
+	
+	@Test
+	public void dummy()
+	{
+		System.out.println("hello manju");
+	}
+
+}
