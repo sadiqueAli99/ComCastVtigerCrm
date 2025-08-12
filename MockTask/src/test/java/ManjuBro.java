4bfbc8639ba6d2a0f5caad5ce1d485601bd13b61
Sadique K.M <66101916+sadiqueAli99@users.noreply.github.com>
2025-08-12 14:53:34 +0530

    updated code

--- MockTask/src/test/java/ManjuBro.java
+++ MockTask/src/test/java/ManjuBro.java
@@ -1,11 +1,9 @@
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.testng.annotations.Test;
 
 public class ManjuBro {
-	
+
 	@Test
-	public void dummy()
-	{
+	public void dummy() {
 		System.out.println("hello manju");
 	}
 
